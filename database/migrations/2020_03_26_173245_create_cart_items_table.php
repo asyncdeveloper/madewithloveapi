9744652ae8c47078de88f8d57eebc863b36ac857
Oluwaseyi Adeogun <sheyilaaw98@gmail.com>
2020-03-26 19:02:38 +0100

    Add migration files for cart and cart items

---
+++ database/migrations/2020_03_26_173245_create_cart_items_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCartItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cart_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('cart_id');
+            $table->foreignId('product_id');
+            $table->unsignedInteger('quantity')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cart_items');
+    }
+}
